83dce046e0539c024ef7b42c76ebd836a48c88b9
Sébastien Menard <sebastien.menard.github@gmail.com>
2024-10-11 15:42:36 -0400

    build(bundle): setup project to produce animation for codepen (#3)

---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import url from 'url';
+import type { Configuration } from 'webpack';
+
+export const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const config: Configuration = {
+    devtool: 'source-map',
+    mode: process.env.NODE_ENV ? 'development' : 'none',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'www'),
+    },
+    plugins: [],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.css', '.scss'],
+        alias: {
+            '@src': path.resolve(__dirname, 'src'),
+        },
+        modules: ['node_modules'],
+    },
+    stats: {
+        errorDetails: true,
+        // logging: 'verbose',
+    },
+};
+
+export default config;
